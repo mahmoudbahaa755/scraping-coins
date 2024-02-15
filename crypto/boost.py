992d37fdc04ac38f9ff649e8fad043f3f2978134
Mahmoud Bahaa <mahmoudbahaa755@gmail.com>
2024-02-16 00:24:20 +0200

    adding crypto

---
+++ crypto/boost.py
@@ -0,0 +1,12 @@
+import threading
+
+
+def boost(callback, assets, interval, depth):
+    thread_list = []
+    for i in range(len(assets)):
+        th = threading.Thread(target = callback, args = (assets[i], interval, depth))
+        thread_list.append(th)
+        th.start()
+    for thrd in thread_list:
+        thrd.join()
+    return thread_list
